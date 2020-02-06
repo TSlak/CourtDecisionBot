357578c936a24cf632cff5a470ed9d51729165bb
Нурутдин Абдулаев <a.nurutdin@dagparus.ru>
2020-02-06 16:26:28 +0300

    i dont know

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
-import telebot
-
+#import telebot
+import FindCourtCase
 # Хранить идентификатор пользователя к каждому делу
-bot = telebot.TeleBot("946595650:AAHPQ9OOR7u3xy3tepfYmaUuaZCgIQ1g3cw")
+# bot = telebot.TeleBot("946595650:AAHPQ9OOR7u3xy3tepfYmaUuaZCgIQ1g3cw")
 
 # bot.send_message("261617836", "Ахмед черт")
 
@@ -14,20 +14,27 @@ bot = telebot.TeleBot("946595650:AAHPQ9OOR7u3xy3tepfYmaUuaZCgIQ1g3cw")
 #
 #
 #
-# def findNewCourtCase(number, date, userId):
-#     if (FindCourtCase.readyThisNumber(number, date, userId)):
-#         print('Такой номер есть')
+
+
+
+def findNewCourtCase(number, date, userId):
+    if (FindCourtCase.readyThisNumber(number, date, userId)):
+        print('Такой номер есть')
+
+    else:
+        link = FindCourtCase.get_link(number, date)
+        print(link)
+    print('----------')
+
+
+findNewCourtCase("", "", "")
+
+
+# print("Hello")
 #
-#     else:
-#         link = FindCourtCase.get_link(number, date)
-#         print(link)
-
-
-print("Hello")
-
-
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+#
+# @bot.message_handler(commands=['start', 'help'])
+# def send_welcome(message):
+#     bot.reply_to(message, "Howdy, how are you doing?")
 
 # findNewCourtCase("", "", "")
