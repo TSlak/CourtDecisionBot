777a591c14cff8491fac2f015d7da2467f6d7a4a
Нурутдин Абдулаев <a.nurutdin@dagparus.ru>
2020-02-07 09:25:27 +0300

    i dont know

--- Main.py
+++ Main.py
@@ -24,6 +24,11 @@ def start(message):
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 
+@bot.message_handler(commands=['findCour'])
+def start(message):
+    bot.reply_to(message, 'Hello111, ' + message.from_user.first_name)
+
+
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     bot.reply_to(message, message.text)
