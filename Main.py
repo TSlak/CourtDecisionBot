acc62e735e99937eaf93c9e4b05e6320701b9fd8
Нурутдин Абдулаев <a.nurutdin@dagparus.ru>
2020-02-07 09:20:56 +0300

    i dont know

--- Main.py
+++ Main.py
@@ -1,17 +1,56 @@
-from telebot import TeleBot
-import request
-from flask import Flask
-import FindCourtCase
-import time
+# from telebot import TeleBot
+# import request
+# from flask import Flask
+# import FindCourtCase
+# import time
 
 # Хранить идентификатор пользователя к каждому делу
-bot = TeleBot("946595650:AAHPQ9OOR7u3xy3tepfYmaUuaZCgIQ1g3cw")
+# bot = TeleBot("946595650:AAHPQ9OOR7u3xy3tepfYmaUuaZCgIQ1g3cw")
+
+
+import os
+
+from flask import Flask, request
+
+import telebot
+
+TOKEN = '946595650:AAHPQ9OOR7u3xy3tepfYmaUuaZCgIQ1g3cw'
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://court-decision-bot.herokuapp.com/' + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+
 # server = Flask("Main")
 # bot.send_message("261617836", "Ахмед черт")
 # bot.polling(none_stop=True)
 
-bot.remove_webhook()
-time.sleep(2)
+# bot.remove_webhook()
+# time.sleep(2)
 
 # bot.set_webhook("https://court-decision-bot.herokuapp.com/" + bot.token)
 
@@ -28,41 +67,24 @@ time.sleep(2)
 #
 
 
-# @server.route("/", methods=['POST'])
-# def getMessage():
-#   r = request.get_json()
-#   if "message" in r.keys():
-#     chat_id = r["message"]["chat"]["id"]
-#     if "text" in r["message"]:
-#       text_mess = r["message"]["text"]
-#     else:
-#       bot.send_message(chat_id=chat_id, text="Какая то не понятная проблема", parse_mode='HTML')
-#       return "ok", 200
+# def findNewCourtCase(number, date, userId):
+#     if (FindCourtCase.readyThisNumber(number, date, userId)):
+#         print('Такой номер есть')
 #
-#   if text_mess == '/start':
-#     bot.send_message(chat_id=chat_id, text="Привет WebHook")
-#     return "ok", 200
-
-
-def findNewCourtCase(number, date, userId):
-    if (FindCourtCase.readyThisNumber(number, date, userId)):
-        print('Такой номер есть')
-
-    else:
-        link = FindCourtCase.get_link(number, date)
-        print(link)
-    print('----------')
-
-
-findNewCourtCase("", "", "")
+#     else:
+#         link = FindCourtCase.get_link(number, date)
+#         print(link)
+#     print('----------')
+#
+#
+# findNewCourtCase("", "", "")
 
 
 # print("Hello")
 #
 #
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+# @bot.message_handler(commands=['start', 'help'])
+# def send_welcome(message):
+#     bot.reply_to(message, "Howdy, how are you doing?")
 
 # findNewCourtCase("", "", "")
-
