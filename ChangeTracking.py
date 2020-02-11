114a77657bf5a19207967b8b2791d2c5442bbfa6
Нурутдин Абдулаев <a.nurutdin@dagparus.ru>
2020-02-11 16:03:56 +0300

    i dont know

--- ChangeTracking.py
+++ ChangeTracking.py
@@ -43,7 +43,7 @@ def check_to_notify(connect, link=None):
         parse_cont2(soup)
         parse_cont3(soup)
         parse_head_case_data(soup)
-        court_result_link = court_link[:court_link.find('/modules')] + parse_court_result_link(soup).get_text(strip=True)
+        court_result_link = court_link[:court_link.find('/modules')] + court_result_link_data
         WorkWithData.insert_court_data(connect, link, cont1_data, cont2_data, cont3_data, head_case_data, court_result_link)
         print("Инсерт")
     else:
@@ -62,7 +62,7 @@ def check_to_notify(connect, link=None):
             parse_cont3(soup)
             parse_head_case_data(soup)
             parse_court_result_link(soup)
-            court_result_link = court_link[:court_link.find('/modules')] + parse_court_result_link(soup).get_text(strip=True)
+            court_result_link = court_link[:court_link.find('/modules')] + court_result_link_data
             i = 0
             messages = messages + 'Номер дела: ' + head_case_data + '\n*Изменены следующие поля: * \n'
             for cont1 in cont1_data.keys():
@@ -113,7 +113,7 @@ def check_to_notify_by_link(connect, link_list):
         parse_cont3(soup)
         parse_head_case_data(soup)
         parse_court_result_link(soup)
-        court_result_link = court_link[:court_link.find('/modules')] + parse_court_result_link(soup).get_text(strip=True)
+        court_result_link = court_link[:court_link.find('/modules')] + court_result_link_data
         i = 0
         messages = messages + 'Номер дела: ' + head_case_data + '\n*Изменены следующие поля: * \n'
         for cont1 in cont1_data.keys():
