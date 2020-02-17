f63390a34bb8696983689ecbbc70f6687ff7a90c
Нурутдин Абдулаев <a.nurutdin@dagparus.ru>
2020-02-17 15:50:34 +0300

    Изменения в архитектуре

--- CourtService.py
+++ CourtService.py
@@ -68,7 +68,7 @@ def form_court_message(court_data):
 
 def subscribe_court_by_call(call):
     text = call.message.html_text.split("\n")
-    link = text[-1][text.find("](") + 2:text.find(")'")]
+    link = text[-1]
     link = link[link.find('="')+2:link.find('">')]
     print(text)
     print(link+"-------123123123")
