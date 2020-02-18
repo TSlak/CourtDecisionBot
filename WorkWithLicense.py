40c330baab7c8a970b87a5a02a5cf8eb956a76c8
Нурутдин Абдулаев <a.nurutdin@dagparus.ru>
2020-02-18 11:30:05 +0300

    Изменения в архитектуре

--- WorkWithLicense.py
+++ WorkWithLicense.py
@@ -8,5 +8,8 @@ def check_license(user_id):
     license_end_date = WorkWithData.get_user_payment_license_date(user_id)
     if license_end_date:
         if current_date < license_end_date:
+            print(license_end_date)
             return True
+        print('False')
+    print('False_2')
     return False
