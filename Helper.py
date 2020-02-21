9cd6bcdec03e21c869ca67f9252280b831b7d44f
Нурутдин Абдулаев <a.nurutdin@dagparus.ru>
2020-02-21 16:56:13 +0300

    Изменения в архитектуре

--- Helper.py
+++
@@ -1,9 +0,0 @@
-import telebot
-
-trial_kb = telebot.types.InlineKeyboardButton("Получить триал (15 дней)", callback_data="get_trial")
-check_payment_kb = telebot.types.InlineKeyboardButton("Проверить подписку", callback_data="check_payment")
-more_data_kb = telebot.types.InlineKeyboardButton("Раскрыть", callback_data="more_data")
-unsubscribe_kb = telebot.types.InlineKeyboardButton("Отписаться", callback_data="unsubscribe")
-less_data_kb = telebot.types.InlineKeyboardButton('Свернуть', callback_data='less_data')
-court_moving_kb = telebot.types.InlineKeyboardButton("Показать движение дела", callback_data='court_moving')
-subscribe_kb = telebot.types.InlineKeyboardButton("Подписаться", callback_data="subscribe")
